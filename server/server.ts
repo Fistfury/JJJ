a9dc27b8851e618fa1c8f2e27379ee55d8f7fa9c
Jonnie Möller <scarabeo85@msn.com>
2024-05-29 14:28:40 +0200

    Added imports

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,14 @@
-const express = require("express");
-const { MongoClient } = require('mongodb');
+import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
+
+const app = express();
+app.get('/', async (req, res) => {
+res.send('Hello World');
+})
+
+app.listen(3000, async () => {
+console.log("Server running on port 3000");
+})
