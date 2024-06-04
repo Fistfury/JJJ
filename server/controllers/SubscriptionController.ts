3c9feb49a58c0daf5cf70144e9c8020cb1af3173
Jennifer <144323213+JenniferLogrell@users.noreply.github.com>
2024-06-04 17:12:05 +0200

    started updating subs but not working

--- server/controllers/SubscriptionController.ts
+++ server/controllers/SubscriptionController.ts
@@ -1,6 +1,7 @@
 import { connectToDatabase } from "../config/db";
 import { Request, Response } from "express";
 import { SubscriptionLevel } from "../models/SubscriptionModel";
+import { ObjectId } from "mongodb";
 
 export const getSubscriptionLevels = async (req: Request, res: Response) => {
     try { 
@@ -16,16 +17,38 @@ export const createSubscription = async (req: Request, res: Response) => {
     try {
     const db = await connectToDatabase();
     const user = db.collection("users");
+    const userExists = await user.findOne({ _id: req.body._id });
 
-    if (user) {
+    if (userExists) {
+        return res.status(409).send("Already subscribed");
+    } else {
         const result = await user.insertOne({"_id": req.body._id, "subscriptionLevel": req.body.subscriptionLevel});
         const createdSubscription = await user.findOne({ _id: result.insertedId });
-        res.status(201).json(createdSubscription);
-        console.log("subscription created", result)
-    } else {
-        res.status(404).send("User not found");
-    }   
+        res.status(201).json(createdSubscription);  
+    } 
     } catch (error) {
         res.status(500).send("Error creating subscription");
-}
-}
+    }
+    };
+
+export const getArticles = async (req: Request, res: Response) => {
+    try {
+      const db = await connectToDatabase();
+      const articles = await db.collection('articles').find({}).toArray();
+      res.json(articles);
+    } catch (error) {
+      res.status(500).send('Error getting articles');
+    }
+  };
+
+
+export const updateSubscription = async (req: Request, res: Response) => {
+    try {
+        const db = await connectToDatabase();
+        const user = db.collection("users");
+        const updatedSubscription = await user.updateOne({ "_id": req.body._id }, { "$set": { "subscriptionLevel": req.body.subscriptionLevel } });
+        res.json(updatedSubscription);
+    } catch (error) {
+        res.status(500).send("Error updating subscription");
+    }
+}; //// NOT WORKING AT ALL 
