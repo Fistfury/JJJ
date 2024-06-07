9fd4f5f12ffcb09d3c06ebeb0a3daadaaab8979e
Jennifer <144323213+JenniferLogrell@users.noreply.github.com>
2024-06-07 14:40:29 +0200

    Started on getting articles but nowhere near finished

---
+++ client/src/Components/DeleteSub.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const DeleteSub = () => {
+    const [subscription, setSubscription] = useState();
+
+
+    return (
+    <div>DeleteSub</div>
+  )
+}
+
+export default DeleteSub
--- server/controllers/SubscriptionController.ts
+++ server/controllers/SubscriptionController.ts
@@ -2,6 +2,22 @@ import { ObjectId } from "mongodb";
 import { connectToDatabase } from "../config/db";
 import { Request, Response } from "express";
 
+interface User {
+    email: string;
+    password: string;
+    stripeCustomerId: string;
+}
+
+interface Admin {
+    email: string;
+    password: string;
+}
+
+interface Session {
+    user? : User;
+    admin? : Admin;
+}
+
 export const getSubscriptionLevels = async (req: Request, res: Response) => {
     try { 
     const db = await connectToDatabase();
@@ -13,19 +29,16 @@ export const getSubscriptionLevels = async (req: Request, res: Response) => {
 };
 
 export const createSubscription = async (req: Request, res: Response) => {
-    const { email, subscriptionLevel } = req.body;
-
-    // uppdatera användare till rätt sub lvl
-    // skapa document i collection subscriptions
-        // länk till användaren
-        // stripe-id
-        // startdatum
+    const { subscriptionLevel } = req.body;
+    let email = (req.session as Session).user?.email 
 
     try {
     const db = await connectToDatabase();
     const user = db.collection("user");
     const collection = db.collection("subscriptions");
     const userExists = await collection.findOne({ "email": email });
+    // TODOJLo testa email hittas
+
 
     if (userExists && userExists.subscriptionLevel === subscriptionLevel) {
         return res.status(409).send("Already subscribed");
@@ -59,11 +72,24 @@ export const createSubscription = async (req: Request, res: Response) => {
         } catch (error) {
             res.status(500).send('Error deleting subscription');
         }
-    };
+    }; 
+    // TODO JLo:
+    // update (ändra status) ist för delete 
+    // göra en noll-lvl 
+    // få in datum 
 
 export const getArticles = async (req: Request, res: Response) => {
-    try { // TODO JLo: get articles sub lvl 
+    try { 
       const db = await connectToDatabase();
+      let email = (req.session as Session).user?.email;
+      
+
+
+      // TODO JLo: get articles sub lvl 
+      // hämta user sub lvl
+      // filtrera artiklar baserat på user sub lvl
+      // lte > less than or equal
+
       const articles = await db.collection('articles').find({}).toArray();
       res.json(articles);
     } catch (error) {
