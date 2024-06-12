a9fc2cfe51a0b802961b3d9390b44e71c690678f
Jennifer <144323213+JenniferLogrell@users.noreply.github.com>
2024-06-12 15:35:14 +0200

    articles working

--- client/src/Pages/Account.tsx
+++ client/src/Pages/Account.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import PauseSub from "../Components/PauseSub"
 
 export const Account = () => {
+
     const [subscription, setSubscription] = useState([]);
         const fetchSubscription = async () => {
             try {
--- server/routes/SubscriptionRoutes.ts
+++ server/routes/SubscriptionRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import {getSubscriptionLevels, createSubscription, pauseSubscription, updateSubscription, /* getArticles */ } from "../controllers/SubscriptionController";
+import {getSubscriptionLevels, createSubscription, pauseSubscription, updateSubscription, getArticles } from "../controllers/SubscriptionController";
 
 const router = Router();
 
 router.get("/", getSubscriptionLevels);
 router.post("/create", createSubscription);
 router.put('/:_id', pauseSubscription);
-//router.get("/articles", getArticles);
+router.get("/articles", getArticles);
 router.put('/update/:_id', updateSubscription);
 
 export default router;
