e0f32801010348437aa725bcfe631dba4c6c1147
Jennifer <144323213+JenniferLogrell@users.noreply.github.com>
2024-06-16 13:11:02 +0200

    pause sub works-ish

--- client/src/Components/PauseSub.tsx
+++ client/src/Components/PauseSub.tsx
@@ -1,13 +1,22 @@
 import { useParams } from 'react-router-dom';
+import { useUser } from '../Contexts/UserContext';
 
 const PauseSub = () => {
-    const { _id } = useParams();
-    const data = {isPaused: true}
+    const data = {isPaused: true};
+    const {user} = useUser();
+    console.log(user);
 
     const handlePause = async () => {
+        if(!user?.subscriptionId) {
+            console.error("user or subscriptionId is undefined")
+            return;
+        }
         if (window.confirm('Are you sure you want to pause your subscription?')) {
-        const response = await fetch(`http://localhost:3000/api/subscriptions/${_id}`, {
+        const response = await fetch(`http://localhost:3000/api/subscriptions/${user?.subscriptionId}`, {
             method: 'PUT',
+            headers: {
+                "content-type": "application/json",
+            },
             credentials: 'include',
             body: JSON.stringify(data),
     });
